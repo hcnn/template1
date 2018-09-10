3d7ded444cd604845d1451cf391c5de763373916
UH <554c46@gmail.com>
2018-09-10 17:37:46 +0200

    github does not support svg in readmes

--- docs/algopic.sh
+++ docs/algopic.sh
@@ -3,7 +3,8 @@
 filename=algopic
 
 pdflatex "${filename}.tex" 
-pdftocairo -svg "${filename}.pdf" "${filename}.svg"
+#pdftocairo -svg "${filename}.pdf" "${filename}.svg"
+pdftocairo -png "${filename}.pdf" "${filename}"
 
 rm "${filename}.aux"
 rm "${filename}.log"
